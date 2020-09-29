e0bfb4c3308d582b88e1fe76b64b4c20748792e5
Fabrício Selotto <37161663+fabricioselotto@users.noreply.github.com>
2020-09-29 01:44:55 -0300

    Add files via upload

---
+++ code/TESTE_5_-_NTP_-_24-09.ino
@@ -0,0 +1,47 @@
+#include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+//#include <WiFi.h> // for WiFi shield
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
+
+const char *ssid     = "Silva";
+const char *password = "familiaselotto321";
+String horario;
+
+WiFiUDP ntpUDP;
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "a.st1.ntp.br",-3* 3600, 60000);
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+
+  horario = timeClient.getFormattedTime();
+
+  Serial.println(horario);
+
+  if(horario == "01:44:00")
+  {
+    Serial.println("Parabens condição funciona");
+  }
+  
+
+
+  delay(1000);
+}
