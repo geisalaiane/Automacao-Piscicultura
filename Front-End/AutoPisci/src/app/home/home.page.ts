465250a45cbdb8a10c1fd54c77b3bce43e8cf078
fabricioselotto <fabricioselottohard01@gmail.com>
2020-10-16 11:01:07 -0300

    ATTFront

--- Front-End/AutoPisci/src/app/home/home.page.ts
+++ Front-End/AutoPisci/src/app/home/home.page.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { ApiService } from '../services/api.service';
+import { Component } from "@angular/core";
+import { ApiService } from "../services/api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-  
   constructor(private apiService: ApiService) {}
 
-  sendMessage(message) {    
+  sendMessage(message) {
     this.apiService.sendMessage(message);
+  }
+
+  disableButton = false;
+
+  disableClick() {
+    this.disableButton = true;
+  }
+
+  enableClick() {
+    this.disableButton = false;
   }
 }
